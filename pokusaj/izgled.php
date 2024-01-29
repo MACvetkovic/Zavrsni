5f9a2ee100717e071a691130c475803d4b58a85d
MACvetkovic <119415891+MACvetkovic@users.noreply.github.com>
2024-01-29 22:29:27 +0100

    Add files via upload

---
+++ pokusaj/izgled.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>enigma</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script2.js" defer></script>
+</head>
+<body>
+    <div class="sredina">
+    <div class="textarea">
+        <textarea></textarea>
+        </div>
+    <div class="masina">
+        <div id="tipke1">
+            <div class="pRed">
+            <button class="gumb" value="Q">Q</button>
+            <button class="gumb" value="W">W</button>
+            <button class="gumb" value="E">E</button>
+            <button class="gumb" value="R">R</button>
+            <button class="gumb" value="T">T</button>
+            <button class="gumb" value="Z">Z</button>
+            <button class="gumb" value="U">U</button>
+            <button class="gumb" value="I">I</button>
+            <button class="gumb" value="O">O</button>
+            <button class="gumb" value="P">P</button>
+            </div>
+            <div class="dRed">
+            <button class="gumb" value="A">A</button>
+            <button class="gumb" value="S">S</button>
+            <button class="gumb" value="D">D</button>
+            <button class="gumb" value="F">F</button>
+            <button class="gumb" value="G">G</button>
+            <button class="gumb" value="H">H</button>
+            <button class="gumb" value="J">J</button>
+            <button class="gumb" value="K">K</button>
+            <button class="gumb" value="L">L</button>
+            </div>
+            <div class="tRed">
+            <button class="gumb" value="Y">Y</button>
+            <button class="gumb" value="X">X</button>
+            <button class="gumb" value="C">C</button>
+            <button class="gumb" value="V">V</button>
+            <button class="gumb" value="B">B</button>
+            <button class="gumb" value="N">N</button>
+            <button class="gumb">M</button>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="spajanje">
+    <div id="tipke">
+            <div class="pRed">
+            <button value="Q">Q</button>
+            <button value="W">W</button>
+            <button value="E">E</button>
+            <button value="R">R</button>
+            <button value="T">T</button>
+            <button value="Z">Z</button>
+            <button value="U">U</button>
+            <button value="I">I</button>
+            <button value="O">O</button>
+            <button value="P">P</button>
+            </div>
+            <div class="dRed">
+            <button value="A">A</button>
+            <button value="S">S</button>
+            <button value="D">D</button>
+            <button value="F">F</button>
+            <button value="G">G</button>
+            <button value="H">H</button>
+            <button value="J">J</button>
+            <button value="K">K</button>
+            <button value="L">L</button>
+            </div>
+            <div class="tRed">
+            <button value="Y">Y</button>
+            <button value="X">X</button>
+            <button value="C">C</button>
+            <button value="V">V</button>
+            <button value="B">B</button>
+            <button value="N">N</button>
+            <button value="M">M</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
